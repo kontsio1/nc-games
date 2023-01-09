c82e66967d803963ffda4e7a9395c948eb749070
kontsio1 <kontsio18@gmail.com>
2023-01-09 15:14:33 +0000

    changed div to li

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom"
  export const ReviewCard = ({review_id, title, owner, review_img_url, votes}) => {
     return (
         <Link to={`/reviews/${review_id}`}>
-            <div>
+            <li>
             <h3>{title}</h3>
             <p> Owner: {owner}</p>
             <img src={review_img_url} alt="Image of the game" width="100" height="100"></img>
             <p> Votes: {votes}</p>
-            </div>
+            </li>
         </Link>
         
     )
