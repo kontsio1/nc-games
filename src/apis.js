8b522a543b37ef9ffc644bb0b295d6e7b0b35cda
kontsio1 <kontsio18@gmail.com>
2023-01-09 14:51:45 +0000

    made changes based on feedback

--- src/apis.js
+++ src/apis.js
@@ -4,7 +4,6 @@ const gamesApi = axios.create({baseURL: 'https://games-selector.onrender.com/api
 
 export const getReviews = () => {
     return gamesApi.get('/reviews').then((res) => {
-        console.log(res.data.reviews)
         return res.data.reviews
     })
 }
--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom"
+
  
- export const ReviewCard = ({title, owner, review_img_url, votes}) => {
+ export const ReviewCard = ({review_id, title, owner, review_img_url, votes}) => {
     return (
-        <div>
+        <Link to={`/reviews/${review_id}`}>
+            <div>
             <h3>{title}</h3>
             <p> Owner: {owner}</p>
-            <img src={review_img_url} width="100" height="100"></img>
+            <img src={review_img_url} alt="Image of the game" width="100" height="100"></img>
             <p> Votes: {votes}</p>
-        </div>
+            </div>
+        </Link>
+        
     )
 }
