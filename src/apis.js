5d6d0873f76da4cb8bcb674309931a6e3c0d6b40
kontsio1 <kontsio18@gmail.com>
2023-01-12 16:08:49 +0000

    finally

--- src/apis.js
+++ src/apis.js
@@ -30,8 +30,10 @@ export const getCategories = ()=>{
     })
 }
 
-export const getReviews = (selectedType)=>{
-    return gamesApi.get('/reviews', {params: {category: selectedType }}).then((res)=>{
+export const getReviews = (selectedType, sortType = 'created_at', sortOrder='desc')=>{
+    console.log(selectedType, sortType, sortOrder, '<< before sending')
+    return gamesApi.get('/reviews', {params: {category: selectedType, sort_by: sortType , order: sortOrder}}).then((res)=>{
+        console.log(res.data.reviews, '<<returning')
         return res.data.reviews
     })
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { getCategories, getReviews, getReviewsByCategory } from "../apis";
 import '../App.css';
+import { ReviewsList } from "./ReviewsList";
 
-export const NavBar = ({setListOfReviews, setSearchParams, searchParams}) => {
+export const NavBar = ({setCategory, setSortBy, sortBy, setOrder, order}) => {
     const [reviewCategories, setReviewCategories] = useState([])
 
     useEffect(()=>{
@@ -12,37 +13,53 @@ export const NavBar = ({setListOfReviews, setSearchParams, searchParams}) => {
         })
     },[])
 
-    const filteredReviews = (slug='') => {
-        setSearchParams({category: slug})
-        getReviews(slug).then((reviews)=>{
-        setListOfReviews(reviews)
-        })
-    }
-
   return (
-    <nav className="dropdown">
-  <button className="dropbtn">Categories</button>
-  <div className="dropdown-content">
-    <ul>
-    <li><button className="categories" onClick={()=>{filteredReviews()}}>All</button></li>
-    {
-    reviewCategories.map(({slug, description})=>{
-        return (
-            <Categories key={Math.random()} slug={slug} description={description} filteredReviews={filteredReviews}/>
-        )
-    })
-    }
-    </ul>
-    </div>
+    <nav className="nav-bar">
+        <div className="dropdown">
+            <button className="dropbtn">Categories</button>
+            <div className="dropdown-content">
+            <ul>
+                <li><button className="categories" onClick={()=>{
+                setCategory('')
+                }}>All</button></li>
+                {
+                reviewCategories.map(({slug, description})=>{
+                return (
+                <Categories key={Math.random()} slug={slug} description={description} setSortBy={setSortBy} setCategory={setCategory}/>
+                )
+                })
+                }
+            </ul>
+            </div>
+        </div>
+        <label> Sort by: </label>
+        <select className="select-container" onChange={(e)=>{
+            setSortBy(e.target.value)
+            }} defaultValue={ sortBy }>
+            <option value={'created_at'}> Date </option>
+            <option value={'owner'}> Owner </option>
+            <option value={'votes'}> Votes </option>
+            <option value={'title'}> Title </option>
+            <option value={'designer'}> Designer </option>
+        </select>
+        <label> Order: </label>
+        <select className="order-container" onChange={(e)=>{
+            setOrder(e.target.value)
+        }} defaultValue={ order }>
+            <option value={'asc'}> ascending </option>
+            <option value={'desc'}> descending </option>
+        </select>
     </nav>
   )
 };
 
-const Categories = ({slug, description, filteredReviews})=>{
+const Categories = ({slug, description, setCategory})=>{
 
     return (
         <li>
-            <button className="categories" onClick={()=>{filteredReviews(slug)}}>{slug}</button>
+            <button className="categories" onClick={()=>{
+                setCategory(slug)
+            }}>{slug}</button>
             <p className="hidden-description">{description}</p>
         </li>
     )
